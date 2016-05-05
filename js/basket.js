0b5fe97113d6327c4bcba5bf30318c6e9261eb5b
dartian <bidauddamien6@gmail.com>
2016-05-05 15:53:52 +0200

    bread discount ok

--- js/basket.js
+++ js/basket.js
@@ -19,18 +19,67 @@
 
     };
 
-    var asDiscount = function(baskets){
-        for(var i = 0; i < baskets; i++){
-            if(baskets[i].name==="Butter" && baskets[i].quantity%2==0){
-                return true;
-            }else if(baskets[i].name==="Milk" && baskets[i].quantity%3==0){
+    var asBreadDiscount = function(baskets){
+        for(var i = 0; i < baskets.length; i++){
+            if(baskets[i].id==0 && baskets[i].quantity%2==0) {
                 return true;
             }
         }
         return false;
     };
 
+    var calculeTotal = function(index, baskets){
+        console.log(index);
+      if(baskets[index].id==1){
+          return milkDiscount(baskets[index]);
+      }else if(baskets[index].id==2){
+          return breadDiscount(index, baskets);
+      }else{
+            return baskets[index].quantity*baskets[index].price;
+      }
+    };
 
+    var milkDiscount = function(product){
+        if(product.id==1 && product.quantity%4==0){
+            return (product.quantity-(product.quantity/4))*product.price;
+        }
+        return product.quantity*product.price;
+    };
+
+    var getBreadQty = function(baskets){
+        for(var i = 0; i < baskets.length; i++){
+            if(baskets[i].id==0) {
+                return baskets[i].quantity;
+            }
+        }
+    };
+
+    var breadDiscount = function(index, baskets){
+        if(asBreadDiscount(baskets)){
+            var i = 1;
+            var qty = getBreadQty(baskets);
+            var total = 0;
+            var nbDiscount = 0;
+            while(i <= qty){
+                if(i%2==0){
+                    nbDiscount++;
+                }
+                i++;
+            }
+            i=0;
+            while(i < baskets[index].quantity){
+                if(i < nbDiscount){
+                    total+=baskets[index].price/2;
+                }else{
+                    total+=baskets[index].price;
+                }
+                i++;
+            }
+            return total;
+        }else{
+            return baskets[index].quantity*baskets[index].price;
+        }
+    };
 
     basket.component("product", {
         templateUrl:"template/product.html",
@@ -51,7 +100,7 @@
             var index = $scope.baskets.indexOf(product);
             if(index>=0){
                 $scope.baskets[index].quantity++;
-                $scope.baskets[index].total= $scope.baskets[index].quantity*$scope.baskets[index].price;
+                $scope.baskets[index].total= calculeTotal(index, $scope.baskets);
             }else{
                 $scope.baskets.push(product);
             }
@@ -73,7 +122,7 @@
 
         $scope.addItem = function(index){
             $scope.baskets[index].quantity++;
-            $scope.baskets[index].total = $scope.baskets[index].quantity*$scope.baskets[index].price;
+            $scope.baskets[index].total = calculeTotal(index, $scope.baskets);
         };
 
         $scope.removeItem = function(index){
@@ -81,7 +130,7 @@
                 $scope.baskets.splice(index, 1);
             }else{
                 $scope.baskets[index].quantity--;
-                $scope.baskets[index].total = $scope.baskets[index].quantity*$scope.baskets[index].price;
+                $scope.baskets[index].total = calculeTotal(index, $scope.baskets);
             }
         };
 
