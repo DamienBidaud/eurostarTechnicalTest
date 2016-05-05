1e881e732b0fac84af5534873c71be1653841669
dartian <bidauddamien6@gmail.com>
2016-05-05 12:33:04 +0200

    init bootstrap + get resources

--- js/basket.js
+++ js/basket.js
@@ -3,4 +3,26 @@
  */
 (function(){
 
+    var basket = angular.module("basketApp", []);
+
+
+    basket.component("product", {
+        
+    });
+
+
+    basket.controller("productsCtrl",  ["$log", "$scope", "$http", function($log, $scope, $http){
+        $log.info("ok");
+        $scope.products = [];
+        $http.get("resource/products.json").then(function(data){
+            $scope.products = data.data;
+        }, function(response){
+           $log.error(response.data);
+           $log.error(response.status);
+        });
+    }]);
+
+    basket.controller("basketCtrl", ["$scope", "$log", function($scope, $log){
+        
+    }])
 })();
